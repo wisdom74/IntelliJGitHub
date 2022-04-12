45197f25b0b914981e042cc31bf57fef9a47dc09
wisdom74 <skawlgp74@gmail.com>
2022-04-12 12:36:48 +0900

    git과 IntelliJ 연동 추가

--- src/Hello.java
+++ src/Hello.java
@@ -2,5 +2,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello Java. Please run.");
         System.out.println("Thank you.");
+        System.out.println("HI");
     }
 }
